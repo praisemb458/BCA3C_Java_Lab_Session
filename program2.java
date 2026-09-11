7cc414447593bd63d6ee37cd0ff73af524b8acb0
Praise Mbeya <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-09-11 14:34:43 +0530

    Delete program2.java

--- program2.java
+++
@@ -1,9 +0,0 @@
-// Write a simple program to display a message
-
-public class program2 {
-    public static void main(String[] args)
-    {
-
-        System.out.println("Hello, World!");
-    }
-}
